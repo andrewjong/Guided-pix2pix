d81a405f496f2d19e53e654bf91be08be3f331b1
Andrew Jong <andrewjong87@gmail.com>
2020-05-01 14:30:52 -0700

    Add training target so that we can finetune on vvt

--- data/vvt_competition_dataset.py
+++ data/vvt_competition_dataset.py
@@ -39,10 +39,12 @@ class VvtCompetitionDataset(BaseDataset):
 
         if opt.isTrain:
             self._keypoints_dir = osp.join(self.root, "lip_train_frames_keypoint")
+            self._frames_dir = osp.join(self.root, "lip_train_frames")
             raise NotImplementedError(
                 "Train is not yet implemented for vvt competition")
         else:
             self._keypoints_dir = osp.join(self.root, "lip_test_frames_keypoint")
+            self._frames_dir = osp.join(self.root, "lip_test_frames")
 
         self.keypoints = glob(f"{self._keypoints_dir}/**/*.json")
         assert len(self.keypoints) > 0
@@ -60,6 +62,16 @@ class VvtCompetitionDataset(BaseDataset):
             mask = np.flip(mask, axis=1).copy()
             mask_target = np.flip(mask_target, axis=1).copy()
         return x, x_target, pose, pose_target, mask, mask_target
+
+    def get_target_frame(self, index):
+        """ Gets the target frame that corresponds to the keypoint at this index """
+        _pose_name = self.keypoints[index]
+        _pose_name.replace("_keypoints.json", ".png")
+        just_folder_and_file = _pose_name.split("/")[-2:]
+        frame_path = osp.join(self._frames_dir, *just_folder_and_file)
+        frame = self.to_tensor(Image.open(frame_path))
+        frame = self._pad_width_up(frame)
+        return frame
 
     def get_input_person_pose(self, index, target_width):
         """from cp-vton, loads the pose as white squares
@@ -85,15 +97,14 @@ class VvtCompetitionDataset(BaseDataset):
             if pointx > 1 and pointy > 1:
                 rr, cc = draw.circle(pointy, pointx, self.radius, shape=(self.img_h, self.img_w))
                 pose_map[i, rr, cc] = 1
-                # assert torch.any(pose_map[i] == 1), "drew but did not find 1s"
 
         # add padding to the w/ h/
-        pad = (target_width - self.img_w)//2
-        pose_map = F.pad(pose_map, (pad, pad), value=-1)  # make the image 256x256
+        pose_map = self._pad_width_up(pose_map, value=-1)# make the image 256x256
         assert all(i == -1 or i == 1 for i in torch.unique(pose_map)), f"{torch.unique(pose_map)}"
         return pose_map
 
-    def _get_person_image_path_from_index(self, index):
+    def _get_input_person_path_from_index(self, index):
+        """ Returns the path to the person image file that is used as input """
         pose_name = self.keypoints[index]
         person_id = pose_name.split("/")[-2]
         folder = osp.join(self._clothes_person_dir, person_id)
@@ -103,24 +114,34 @@ class VvtCompetitionDataset(BaseDataset):
         assert person_image_name.endswith(".png"), f"person images should have .png extensions: {person_image_name}"
         return osp.join(folder, person_image_name)
 
-    def get_person_image(self, index):
-        pers_image_path = self._get_person_image_path_from_index(index)
+    def _pad_width_up(self, tensor, value=0, original=192, new=256):
+        if original > new:
+            raise ValueError("This function can only pad up if the original size is smaller than the new size")
+        pad = (new - original) // 2
+        new_tensor = F.pad(tensor, (pad, pad), value=value)
+        return new_tensor
+
+    def get_input_person(self, index):
+        """An index specifies the keypoint; get the """
+        pers_image_path = self._get_input_person_path_from_index(index)
         person_image = Image.open(pers_image_path)
         person_tensor = self.to_tensor(person_image)
-        pad = (256 - 192) // 2
-        person_tensor = F.pad(person_tensor, (pad, pad))
+        person_tensor = self._pad_width_up(person_tensor)
         return person_tensor
 
 
     def __getitem__(self, index):
-        image = self.get_person_image(index)  # (3, 256, 256)
+        image = self.get_input_person(index)  # (3, 256, 256)
         try:
             pose_target = self.get_input_person_pose(index, target_width=256)  # (18, 256, 256)
         except IndexError as e:
             print(e.__traceback__)
             print(f"[WARNING]: no pose found {self.keypoints[index]}")
             pose_target = torch.zeros(18, 256, 256)
+        image_target = self.get_target_frame(index)
+
         assert image.shape[-2:] == pose_target.shape[-2:], f"hxw don't match: image {image.shape}, pose {pose_target.shape}"
+        assert image.shape[-2:] == image_target.shape[-2:], f"hxw don't match: image {image.shape}, pose {pose_target.shape}"
 
         # random fliping
         # if (self.opt.isTrain):
@@ -134,9 +155,9 @@ class VvtCompetitionDataset(BaseDataset):
         # input-guide-target
         input = (image / 255) * 2 - 1
         guide = pose_target
-        # target = (x_target / 255) * 2 - 1
+        target = (image_target / 255) * 2 - 1
         # Put data into [input, guide, target]
-        return {'A': input, "guide_path": self.keypoints[index], 'guide': guide, 'B': torch.zeros(3, 256, 256)}
+        return {'A': input, "guide_path": self.keypoints[index], 'guide': guide, 'B': target}
 
     def __len__(self):
         return len(self.keypoints)
